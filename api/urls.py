02a372bc51b6e3506a454f5cdf73cf1423a3e27c
paxthree <ewangjones@btinternet.com>
2018-01-25 19:36:17 +0000

    add api functionality

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='api'),
-    path('businesses/', views.ViewBusinesses, name='businesses')
+    path('business/', views.BusinessView.as_view(), name='business'),
+    path('deals/', views.DealView.as_view(), name='deal'),
+    path('user/', views.Signin, name='user'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views import View
 
 import json
 
 from .models import Business
 
+
+
 def index(request):
     return HttpResponse("Hello World")
 
-def ViewBusinesses(request):
-    businesses = Business.objects.all().values()
-    result = {'items': []}
 
-    for b in businesses:
-        result['items'].append(b)
+def Signin(request):
+    email = request.GET.get['email']
 
-    return JsonResponse(result)
+
+class BusinessView(View):
+    def get(self, request):
+        businesses = Business.objects.all().values()
+        result = {'items': []}
+
+        for b in businesses:
+            result['items'].append(b)
+
+        return JsonResponse(result)
+
+
+class DealView(View):
+    def get(self, request):
+        deals = Business.objects.all().values()
+        result = {'items': []}
+
+        for d in deals:
+            result['items'].append(d)
+
+        return JsonResponse(result)
