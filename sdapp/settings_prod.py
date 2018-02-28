30e28e81e425cf953a10723b5f05cb9f2129985b
Ewan <ewangjones@btinternet.com>
2018-02-28 19:52:53 +0000

    update settings prod

--- sdapp/settings_prod.py
+++ sdapp/settings_prod.py
@@ -16,7 +16,24 @@ DATABASES = {
     }
 }
 
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/root/webapps/logs/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sdapp.settings_prod')
